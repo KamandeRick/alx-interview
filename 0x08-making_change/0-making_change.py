5a1bf7ec16f2d50ce05a206c779bf0650685fe93
kamanderick <rickmwangi237@gmail.com>
2025-01-09 11:48:24 +0000

    add function to determine least change needed

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+
+def makeChange(coins, total):
+    """
+    Function to determine the min number of coins needed to equal
+    sum of a given total amount.
+    Args:
+        coins (list): List containing coin denominations (integers > 0)
+        total (int): Target amount to make change for
+    Returns:
+        int: Min number of coins needed, -1 if impossible,
+        0 if total <= 0
+    """
+
+    if total <= 0:
+        return 0
+    if not coins:
+        return -1
+
+    coins_needed = [total + 1] * (total + 1)
+    coins_needed[0] = 0
+
+    for amount in range(1, total + 1):
+
+        for coin in coins:
+            if coin <= amount:
+
+                coins_needed[amount] = min(
+                        coins_needed[amount],
+                        coins_needed[amount - coin] + 1
+                        )
+
+    return -1 if coins_needed[total] > total else coins_needed[total]
